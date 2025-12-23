abe41eeddba7018900fd0d9e5b48ba2a4ee0b410
tereschvlad <tereschvlad@gmail.com>
2025-12-23 02:21:19 +0200

    correction

--- Models/ConnectionDataOption.cs
+++ Models/ConnectionDataOption.cs
@@ -2,28 +2,28 @@ namespace DeferDataLoading;
 
 internal class ConnectionDataOption
 {
-    public string DbName { get; set; }
+    public string DbName { get; init; }
 
-    public string DbConnection { get; set; }
+    public string DbConnection { get; init; }
 
-    public string MongoDbConnection { get; set; }
+    public string MongoDbConnection { get; init; }
 
-    public string MongoDbName { get; set; }
+    public string MongoDbName { get; init; }
 
-    public string QueueName { get; set; }
+    public string QueueName { get; init; }
 
-    public string RabbitMqHostName { get; set; }
+    public string RabbitMqHostName { get; init; }
 
-    public int RabbitMqPort { get; set; }
+    public int RabbitMqPort { get; init; }
 
-    public string RabbitMqUserName { get; set; }
+    public string RabbitMqUserName { get; init; }
 
-    public string RabbitMqPassword { get; set; }
+    public string RabbitMqPassword { get; init; }
 
-    public string SeqKey { get; set; }
+    public string SeqKey { get; init; }
 
-    public string SeqHost { get; set; }
+    public string SeqHost { get; init; }
 
-    public int WorkerDelayed { get; set; }
+    public int WorkerDelayed { get; init; }
 
 }
